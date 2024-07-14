63644d70bbbcbe7b2c8f0c044e54d84554d67fef
ross <leeperross@gmail.com>
2024-07-14 22:42:56 +0100

    All front end functioning aside from only sending form when main button is clicked. Next step in chatgpt.

--- app.py
+++ app.py
@@ -19,10 +19,36 @@ def search_results():
     # Perform the search and get results
     # For demonstration, we'll use a placeholder list of results
     results = [
-        {"title": "Example Channel 1", "description": "This is a description for Example Channel 1."},
-        {"title": "Example Channel 2", "description": "This is a description for Example Channel 2."}
+        {"title": "Example Channel 1", "description": "This is a description for Example Channel 1.", "id": "12345"},
+        {"title": "Example Channel 2", "description": "This is a description for Example Channel 2.", "id": "23456"},
+        {"title": "Example Channel 3", "description": "This is a description for Example Channel 3.", "id": "34567"},
+        {"title": "Example Channel 4", "description": "This is a description for Example Channel 4.", "id": "45678"},
+        {"title": "Example Channel 5", "description": "This is a description for Example Channel 5.", "id": "56789"},
     ]
     return render_template('channel_results.html', query=query, results=results)
+
+
+@app.route('/video_search', methods=['GET'])
+def video_search():
+    title = request.args.get('title', 'Default Title')
+    date_option = request.args.get('date_option', 'all-time')
+    from_date = request.args.get('from_date', '')
+    to_date = request.args.get('to_date', '')
+    for_each = request.args.get('for_each', 'year')
+    top = request.args.get('top', '1')
+    sort_option = request.args.get('sort_option', 'newest')
+
+    # Logic to filter videos based on search options
+    # For demonstration, using static video list
+    videos = [{'title': 'Sample Video 1', 'description': 'Description 1'},
+              {'title': 'Sample Video 2', 'description': 'Description 2'}]
+
+    return render_template('video_results.html', title=title, videos=videos)
+
+
+@app.route('/faqs')
+def faqs():
+    return render_template('faqs.html')
 
 
 if __name__ == '__main__':
